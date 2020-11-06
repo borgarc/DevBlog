2ddc4ddb14eea967564942194792ee82d1791855
borgarc <borjagarciavillanueva@gmail.com>
2020-11-06 16:00:53 +0100

    Added foreign keys to post model to allow videos and images

--- posts/models.py
+++ posts/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from people.models import Person
+from videos.models import Video
+from images.models import Images
 
 class Post(models.Model):
     content = models.CharField(max_length=1000)
     creation_date = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(Person, on_delete=models.CASCADE)
+    image = models.ForeignKey(Images, null=True, on_delete=models.CASCADE)
+    video = models.ForeignKey(Video, null=True, on_delete=models.CASCADE)
 
--- posts/serializers.py
+++ posts/serializers.py
@@ -4,4 +4,4 @@ from posts.models import Post
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ('id', 'content', 'creation_date', 'user')
+        fields = ('id', 'content', 'creation_date', 'user', 'image', 'video')
