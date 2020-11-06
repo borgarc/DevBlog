8bbb08f324e1fa32ec3e19ca86dfa3e707d0e046
borgarc <borjagarciavillanueva@gmail.com>
2020-11-06 14:20:09 +0100

    Merge conflicts

--- devblogback/settings.py
+++ devblogback/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'images.apps.ImagesConfig',
     'comments.apps.CommentsConfig',
     'posts.apps.PostsConfig',
     'people.apps.PeopleConfig',
